74bc4f99731c83297ab8e3bbf6675709ccd56c78
legendchild <sagejisung@gmail.com>
2023-01-01 19:32:54 +0900

    수정

--- include.js
+++ include.js
@@ -1,33 +1,27 @@
-function includeHTML(){
-    let z, elmnt, file, xhttp;
- 
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /* Loop through a collection of all HTML elements: */
     z = document.getElementsByTagName("*");
-    
-    for (let i = 0; i < z.length; i++) {
+    for (i = 0; i < z.length; i++) {
       elmnt = z[i];
-      file = elmnt.getAttribute("data-include");
-      
+      /*search for elements with a certain atrribute:*/
+      file = elmnt.getAttribute("include-html");
       if (file) {
+        /* Make an HTTP request using the attribute value as the file name: */
         xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
           if (this.readyState == 4) {
             if (this.status == 200) {elmnt.innerHTML = this.responseText;}
             if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
             /* Remove the attribute, and call this function once more: */
-            elmnt.removeAttribute("data-include");
+            elmnt.removeAttribute("include-html");
             includeHTML();
-          }//if
-        }//onreadystatechange
- 
+          }
+        }
         xhttp.open("GET", file, true);
         xhttp.send();
+        /* Exit the function: */
         return;
-      }//if - file
-    }//for
-}//includeHTML
- 
- 
-/* 실행 */
-window.addEventListener('DOMContentLoaded',()=>{
-    includeHTML();
-});
+      }
+    }
+  }
